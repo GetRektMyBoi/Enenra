fce8e00a96c5b6d51f2b30c4de496c912ea9f9e4
Dnami_ <39431844+GetRektMyBoi@users.noreply.github.com>
2018-05-20 15:30:08 +0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+const prefix = config.prefix;
+
+client.on('ready", () => {
+	console.log('Logged in as ${client.user.tag}');
+	client.user.setActivity(config.defualtActivity);
+});
+
+client.on('message', msg =>{
+	if (msg.author.bot) return;
+	if(msg.content.indexOf(config.prefix) !== 0) return;
+	
+	const args = msg.content.slice(config.prefix.length).trim().split(/+/g);
+	const command = args.shift().toLowerCase();
+	
+	if (command ==='ping') {
+	msg.channel.send('Pong!');
+	} else {
+		return;
+	}
+});
+
+client.login(process.env.token);
